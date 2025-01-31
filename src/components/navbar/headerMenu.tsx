b3aae247c46c6e75061f10d012615d9ad9ac08df
Abeltony-prog <abeltony03@gmail.com>
2025-01-31 17:28:49 +0200

    innitial commit

--- src/components/navbar/headerMenu.tsx
+++ src/components/navbar/headerMenu.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default function HeaderMenu(){
@@ -8,10 +9,18 @@ export default function HeaderMenu(){
             <img src="/assets/images/logo/logo.png" alt="Planet" className="h-12" />
           </div>
           <nav className="hidden md:flex items-center space-x-8">
-            <a href="#" className="text-red-600 font-medium">Home</a>
-            <a href="#" className="text-[#2B3990] hover:text-red-600">Our Services</a>
-            <a href="#" className="text-[#2B3990] hover:text-red-600">About Us</a>
-            <a href="#" className="text-[#2B3990] hover:text-red-600">Contact Us</a>
+            <Link href={'/'} passHref>
+            <span className="text-red-600 font-medium">Home</span>
+            </Link>
+            <Link href={'/Services'} passHref>
+            <span className="text-[#2B3990] cursor-pointer hover:text-red-600">Our Services</span>
+            </Link>
+            <Link href={'/AboutUs'} passHref>
+            <span className="text-[#2B3990] cursor-pointer hover:text-red-600">About Us</span>
+            </Link>
+            <Link href={'/ContactUs'} passHref>
+            <span className="text-[#2B3990] hover:text-red-600">Contact Us</span>
+            </Link>
           </nav>
           <button className="md:hidden">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
