d50f232ecfadd5b6987073bea811bddf0e411d20
Abeltony-prog <abeltony03@gmail.com>
2025-02-05 17:20:40 +0200

    innitial commit

--- pages/ContactUs/index.tsx
+++ pages/ContactUs/index.tsx
@@ -37,6 +37,60 @@ export default function ContactUsPage() {
                 <p className="text-gray-600">example@test.com</p>
               </div>
             </div>
+            <div>
+              <a
+                href="https://www.google.com/maps/place/Planet+Events+Group/@-1.9578391,30.1132854,18z/data=!4m6!3m5!1s0x19dca7a4962c617f:0x23299e39cb94495b!8m2!3d-1.9587652!4d30.1149355!16s%2Fg%2F11wppnf73p?entry=ttu&g_ep=EgoyMDI1MDIwMi4wIKXMDSoASAFQAw%3D%3D"
+                target="_blank"
+                className="absolute rounded-lg bg-red-500  py-2 px-6 text-white hover:cursor-pointer"
+              >
+                <span className="flex">
+                  <svg
+                    width="15px"
+                    className="mt-1 pr-1"
+                    height="15px"
+                    viewBox="-4 0 32 32"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#000000"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      {" "}
+                      <title>location</title>{" "}
+                      <desc>Created with Sketch Beta.</desc> <defs> </defs>{" "}
+                      <g
+                        id="Page-1"
+                        stroke="none"
+                        stroke-width="1"
+                        fill="none"
+                        fill-rule="evenodd"
+                      >
+                        {" "}
+                        <g
+                          id="Icon-Set"
+                          transform="translate(-104.000000, -411.000000)"
+                          fill="#ffffff"
+                        >
+                          {" "}
+                          <path
+                            d="M116,426 C114.343,426 113,424.657 113,423 C113,421.343 114.343,420 116,420 C117.657,420 119,421.343 119,423 C119,424.657 117.657,426 116,426 L116,426 Z M116,418 C113.239,418 111,420.238 111,423 C111,425.762 113.239,428 116,428 C118.761,428 121,425.762 121,423 C121,420.238 118.761,418 116,418 L116,418 Z M116,440 C114.337,440.009 106,427.181 106,423 C106,417.478 110.477,413 116,413 C121.523,413 126,417.478 126,423 C126,427.125 117.637,440.009 116,440 L116,440 Z M116,411 C109.373,411 104,416.373 104,423 C104,428.018 114.005,443.011 116,443 C117.964,443.011 128,427.95 128,423 C128,416.373 122.627,411 116,411 L116,411 Z"
+                            id="location"
+                          >
+                            {" "}
+                          </path>{" "}
+                        </g>{" "}
+                      </g>{" "}
+                    </g>
+                  </svg>
+                  Use Google Map
+                </span>
+              </a>
+            </div>
           </div>
 
           {/* Contact Form */}
